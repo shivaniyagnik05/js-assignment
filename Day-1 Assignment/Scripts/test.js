10961a7024a6af29ceca047749a07e71593bc848
shivaniyagnik05 <68267497+shivaniyagnik05@users.noreply.github.com>
2020-07-15 14:03:47 +0530

    Add files via upload

---
+++ Day-1 Assignment/Scripts/test.js
@@ -0,0 +1,21 @@
+/*console.log: 
+        it is used for log(print) the output in the console
+*/
+console.log("Hey how are you all");
+
+/*console.error:
+        it is used to display error message in console
+*/
+console.error("This is error message");
+/*
+console.warn:
+        it is used to display warning message in console
+*/
+console.warn("This is Warning");
+
+var a={name:"xyz",age:"20",city:"Mumbai"}
+var b = {name:"abc", age:"25",city:"Ahmedabad"}
+
+console.table({a,b});
+
+
---
+++ Day-1 Assignment/Scripts/varletconst.js
@@ -0,0 +1,21 @@
+//var
+var name="shivani";
+var age="23";
+var city="Mumbai";
+
+console.log(name,age,city);
+
+//let
+{
+    let name="shivani";
+name="vikas";
+console.log(name);
+}
+console.log(name)
+
+//const
+
+const mobile=["Samsung","Vivo","Apple","OnePlus"]
+console.log(mobile)
+mobile.push("Realme")
+console.log(mobile)
